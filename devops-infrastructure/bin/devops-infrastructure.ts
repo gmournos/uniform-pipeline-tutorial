d72aace107b729a94aac3fbfbaa438b30d541f9e
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-11-13 11:39:21 +0100

    22 export the builder

--- devops-infrastructure/bin/devops-infrastructure.ts
+++ devops-infrastructure/bin/devops-infrastructure.ts
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { DevopsInfrastructureStack } from '../lib/devops-infrastructure-stack';
+import { CrossRegionSupportStackBuilder } from '../lib/cross-region-support-stack-builder';
 
 const app = new cdk.App();
-new DevopsInfrastructureStack(app, 'devops-infrastructure-stack', {
-});
+const buildSupport = app.node.tryGetContext('support');
+
+if (buildSupport === 'true') {
+    new CrossRegionSupportStackBuilder(app);
+} else {
+    new DevopsInfrastructureStack(app, 'devops-infrastructure-stack', {});
+}
