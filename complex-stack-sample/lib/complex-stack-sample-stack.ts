0f3cfb22d5f5564094502f8f3a2f4a4d5553b4a0
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-11-13 05:29:29 +0100

    7 add a bucket deployment

--- complex-stack-sample/lib/complex-stack-sample-stack.ts
+++ complex-stack-sample/lib/complex-stack-sample-stack.ts
@@ -1,8 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 import { Construct } from 'constructs';
 
 export class ComplexStackSampleStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
+
+        // 1. Create an S3 Bucket
+        const bucket = new Bucket(this, 'sample-bucket', {
+            versioned: true,
+            bucketName: `uniform-pipelines-sample-bucket-${this.account}-${this.region}`,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+        });
+
+        // 2. Deploy a sample file to the S3 Bucket
+        new BucketDeployment(this, 'deploy-sample-file', {
+            sources: [Source.asset('./sample-file')],
+            destinationBucket: bucket,
+        });
     }
 }
