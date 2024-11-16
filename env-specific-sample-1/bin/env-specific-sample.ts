8ae305c1e083a0239470ad189d84496bc1f0e1b0
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-11-16 19:03:12 +0100

    82 into the pipelines

--- env-specific-sample-1/bin/env-specific-sample.ts
+++ env-specific-sample-1/bin/env-specific-sample.ts
@@ -1,11 +1,23 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { EnvSpecificSampleStack } from '../lib/env-specific-sample-stack';
+import { InnerPipelineStackFactory } from '@uniform-pipelines/in-factory';
+import { EnvSpecificSampleStack, EnvSpecificSampleStackProps } from '../lib/env-specific-sample-stack';
 
 const app = new cdk.App();
 
-new EnvSpecificSampleStack(app, 'env-specific-1', {
-    description: "Environment specific stack",
-    environmentName: 'development',
-});
+const inPipelines = app.node.tryGetContext('pipeline');
+const containedStackDescription = app.node.tryGetContext('description');
+const containedStackVersion = app.node.tryGetContext('version');
+const containedStackName = app.node.tryGetContext('stackName');
 
+const versionedDescription = `${containedStackName}:${containedStackVersion}: ${containedStackDescription}`;
+
+if (inPipelines === 'true') {
+    new InnerPipelineStackFactory<EnvSpecificSampleStackProps>().buildInnerPipelineStackBase(app, EnvSpecificSampleStack);
+} else {
+    new EnvSpecificSampleStack(app, 'ComplexStackSampleStack', {
+        description: versionedDescription,
+        stackName: containedStackName,
+        environmentName: 'development',
+    });
+}
