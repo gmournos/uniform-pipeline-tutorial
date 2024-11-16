9c6936aadfe4bf655adf0df8e7666a74ce129d54
MOURNOS Georgios <georgios.mournos@ext.ec.europa.eu>
2024-11-16 15:43:30 +0100

    75 factory class

--- complex-stack-sample/bin/complex-stack-sample.ts
+++ complex-stack-sample/bin/complex-stack-sample.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import { ComplexStackSampleStack } from '../lib/complex-stack-sample-stack';
-import { makeVersionedPipelineStackName, INNER_PIPELINE_STACK_TEMPLATE_NAME, TargetEnvironments } from '@uniform-pipelines/model';
-import { InnerPipelineStack } from '../lib/inner-pipeline-stack';
+import { InnerPipelineStackFactory } from '../lib/inner-pipeline-stack-factory';
 
 const app = new cdk.App();
 
@@ -14,22 +13,9 @@ const containedStackName = app.node.tryGetContext('stackName');
 const versionedDescription = `${containedStackName}:${containedStackVersion}: ${containedStackDescription}`;
 
 if (inPipelines === 'true') {
-    new InnerPipelineStack(app, INNER_PIPELINE_STACK_TEMPLATE_NAME, {
-        containedStackName,
-        containedStackVersion: containedStackVersion,
-        containedStackClass: ComplexStackSampleStack,
-        containedStackProps: {
-            description: versionedDescription,
-        },
-        description: 'Pipeline stack to deploy Feature3',
-        stackName: makeVersionedPipelineStackName(containedStackName, containedStackVersion),
-        env: {
-            account: TargetEnvironments.DEVOPS.account,
-            region: TargetEnvironments.DEVOPS.region,
-        }
-    });
+    new InnerPipelineStackFactory().buildInnerPipelineStackBase(app, ComplexStackSampleStack);
 } else {
     new ComplexStackSampleStack(app, 'ComplexStackSampleStack', {
-        description: containedStackDescription,
+        description: versionedDescription,
     });
 }
---
+++ complex-stack-sample/lib/inner-pipeline-stack-factory.ts
@@ -0,0 +1,32 @@
+import { StackProps } from "aws-cdk-lib";
+import { INNER_PIPELINE_STACK_TEMPLATE_NAME, makeVersionedPipelineStackName, TargetEnvironments } from "@uniform-pipelines/model";
+import { InnerPipelineStack } from "./inner-pipeline-stack";
+import { ContainedStackClassConstructor } from "./inner-pipeline-construct";
+import { Construct } from "constructs";
+
+export class InnerPipelineStackFactory<P extends StackProps = StackProps> {
+    buildInnerPipelineStackBase(parentScope: Construct, containedStackClass: ContainedStackClassConstructor<P>, extraContainedStackProps?: Record<string, any>) {
+        const containedStackVersion = parentScope.node.tryGetContext('version');
+        const containedStackName = parentScope.node.tryGetContext('stackName');
+        const containedStackDescription = parentScope.node.tryGetContext('description');
+        const versionedDescription = `${containedStackName}:${containedStackVersion}: ${containedStackDescription}`;
+        const innerPipelineConstructProps = {
+            containedStackName,
+            containedStackVersion,
+            containedStackClass,
+            containedStackProps: {
+                ...extraContainedStackProps,
+                description: versionedDescription,
+            } as P,
+        };
+        new InnerPipelineStack(parentScope, INNER_PIPELINE_STACK_TEMPLATE_NAME, {
+            ...innerPipelineConstructProps,
+            description: `Inner Delivery Pipeline for ${containedStackName}:${containedStackVersion}`,
+            stackName: makeVersionedPipelineStackName(containedStackName, containedStackVersion),
+            env: {
+                account: TargetEnvironments.DEVOPS.account,
+                region: TargetEnvironments.DEVOPS.region,
+            }
+        });
+   }
+}
